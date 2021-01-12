1daf182b8ecbd0ee60e298908bf6a70d57096c84
Gowtham <70662123+muralispaiitm@users.noreply.github.com>
2021-01-12 19:52:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -111,10 +111,9 @@ def predict_rf_model_feature_10_single_file():
         # -------------------------- Predicting the result --------------------------
         X['result'] = predict_df(df)
         # ------------------ Storing the result into specific path ------------------
-        # X.to_csv(file_path, index=False)
         X.to_csv(file_path, index=False)
 
-    return render_template("result_page.html", type="single_file", file_name=file_name, result="Successfully Predicted", DbMessage='Locally stored', path=type(X))
+    return render_template("result_page.html", type="single_file", file_name=file_name, result="Successfully Predicted", DbMessage='Locally stored', path=file_path)
 
 
 # Using Batch Files : ---------------------------------------------------------------------------
@@ -147,11 +146,10 @@ def predict_rf_model_feature_10_batch_files():
             X['result'] = predict_df(df)
             # ----------------------- Storing the resultant files -----------------------
             predicted_files.append(file_name)
-            # files_store_path = os.getcwd() + '/Data/Batch_Files/Predicting_Files/'
+            # file_store_path = os.getcwd() + '/Data/Batch_Files/Predicting_Files/'
             path = "C:\\Users\\mural\\OneDrive\\Documents\\GitHub\\MOOC_Student_Drop-Projects\\Data\\Batch_Files\\Predicting_Files"
             file_store_path = os.path.join(path, file_name).replace('/', '\\')  # Location of the file stored
-            # file_store_path = file_store_path.replace('/', '\\')
-            X.to_csv(file_store_path, index=False)
+            # X.to_csv(file_store_path, index=False)
     #return render_template("result_page.html", type="batch_files", skipped_files=skipped_files, predicted_files=predicted_files, DbMessage='Locally Stored', path=os.path.split(file_store_path)[0])
 
     return render_template("result_page.html", type="batch_files", skipped_files=skipped_files, predicted_files=predicted_files, DbMessage='Locally Stored', path=path)
